3fa74dc300dcb8f05f9a65b5d73324271e6220ab
Galenc <17chungg51@millshighsch.org>
2016-10-18 10:25:01 -0700

    Hourglass
    Finished Tophalf of Hourglass

---
+++ Collaborative Programming Exercise/src/ProcessingNumbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ProcessingNumbers {
+	public static void main (string[] args){
+		Scanner console = new Scanner("System:n");
+		System.out.println("enter 2 numbers");
+		int max=Console.next int();
+		int min=Console.next int();
+		id(min,max){
+			System.out.println("min");
+		}else{
+			System.out.println("max");
+		}
+			System.out.println("min+Max");
+		}
+	}
+	
+
